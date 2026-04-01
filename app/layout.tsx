9489aa281d2623a6ebf5dd74ffa8b0ef1e15126d
Web Vanguard AI <webvanguardai@gmail.com>
2026-04-01 11:01:44 +0400

    feat: use beaker image as favicon + apple-touch-icon

--- app/layout.tsx
+++ app/layout.tsx
@@ -75,8 +75,9 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
-        <link rel="icon" href="/favicon.svg" type="image/svg+xml" />
+        <link rel="icon" href="/favicon.png" type="image/png" sizes="32x32" />
         <link rel="icon" href="/favicon.ico" sizes="any" />
+        <link rel="apple-touch-icon" href="/apple-icon.png" sizes="180x180" />
         <script
           type="application/ld+json"
           dangerouslySetInnerHTML={{
